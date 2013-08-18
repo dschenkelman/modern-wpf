e90580194c2606bb93233d84c36d92993be5314c
dschenkelman <damian.schenkelman@gmail.com>
2013-08-18 18:15:43 -0300

    # Entity framework local demo

---
+++ src/ModenWPF.EntityFramework/ModenWPF.EntityFramework/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ModenWPF.EntityFramework
+{
+    using System.Data.Entity;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Database.SetInitializer(new MarketInitializer());
+        }
+    }
+}
