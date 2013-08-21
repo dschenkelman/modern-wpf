eacedf907adfe7c9ce7c7c16b9da758d4fd7c72e
Mariano Converti <mariano.converti@southworks.net>
2013-08-21 00:01:29 -0300

    # added PCL demo (WIP)

---
+++ src/ModernWPF.PCL/ModernWPF.PCL.Common/Client/CompaniesClient.cs
@@ -0,0 +1,43 @@
+﻿namespace ModernWPF.PCL.Common.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using ModernWPF.PCL.Common.Model;
+    using Newtonsoft.Json;
+
+    public class CompaniesClient
+    {
+        private const string CompaniesUrl = "/api/companies";
+
+        private readonly Uri serviceBaseUri;
+
+        public CompaniesClient(string serviceBaseUrl)
+        {
+            this.serviceBaseUri = new Uri(serviceBaseUrl, UriKind.Absolute);
+        }
+
+        public async Task<IEnumerable<Company>> GetCompaniesAsync()
+        {
+            using (var client = new HttpClient { BaseAddress = this.serviceBaseUri })
+            {
+                var response = await client.GetAsync(CompaniesUrl);
+                var companiesJsonResponse = await response.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<IEnumerable<Company>>(companiesJsonResponse);
+            }
+        }
+
+        public async Task<bool> PostCompany(Company company)
+        {
+            using (var client = new HttpClient { BaseAddress = this.serviceBaseUri })
+            {
+                var response = await client.PostAsync(CompaniesUrl, new StringContent(JsonConvert.SerializeObject(company)));
+
+                return response.IsSuccessStatusCode;
+            }
+
+        }
+    }
+}
